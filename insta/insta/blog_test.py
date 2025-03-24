ff190c4a2bd2e6683029b2dc0a476bfba2a8e4df
juneejun23 <juneejun6086@gmail.com>
2025-03-24 17:10:12 +0900

    기존 홈 페이지 템플릿 삭제 및 블로그 포스트 날짜 형식 개선, 댓글 기능 추가, 이미지 미리보기 기능 구현

--- insta/insta/blog_test.py
+++ insta/insta/blog_test.py
@@ -1,25 +1,51 @@
 from flask import Flask, render_template, request, redirect, jsonify
 from werkzeug.utils import secure_filename
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 
+from datetime import datetime
+
+# 문자열을 datetime 객체로 변환
+def parse_date(date_str):
+    return datetime.strptime(date_str, '%B %d, %Y')
+
 # 임시 데이터
 posts = [
-    {'id': 1, 'title': '파이썬이란?', 'content': 'Python은 웹 애플리케이션, 소프트웨어 개발, 데이터 과학, 기계 학습에 널리 사용되는 프로그래밍 언어입니다.', 'image_url': '/static/images/python_img.jpg', 'date': 'MARCH 15, 2025'},
-    {'id': 2, 'title': '자바스크립트란?', 'content': '자바스크립트는 웹 개발에서 가장 널리 사용되는 프로그래밍 언어입니다.', 'image_url': '/static/images/java_img.png', 'date': 'MARCH 16, 2025'},
+    {'id': 1, 'title': '파이썬이란?', 'content': 'Python은 웹 애플리케이션, 소프트웨어 개발, 데이터 과학, 기계 학습에 널리 사용되는 프로그래밍 언어입니다.', 'image_url': '/static/images/python_img.jpg', 'date': parse_date('MARCH 15, 2025')},
+    {'id': 2, 'title': '자바스크립트란?', 'content': '자바스크립트는 웹 개발에서 가장 널리 사용되는 프로그래밍 언어입니다.', 'image_url': '/static/images/java_img.png', 'date': parse_date('MARCH 16, 2025')},
 ]
+
 
 @app.route('/')
 def home():
-    return render_template('another_blog.html', posts=posts)
+    sorted_posts = sorted(posts, key=lambda x: x['date'], reverse=True)
+    
+    # 날짜를 문자열로 변환
+    for post in sorted_posts:
+        if isinstance(post['date'], datetime):
+            post['date_str'] = post['date'].strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            post['date_str'] = post['date']
+
+    return render_template('another_blog.html', posts=sorted_posts)
+
 
 @app.route('/post/<int:post_id>')
 def post(post_id):
     post = next((p for p in posts if p['id'] == post_id), None)
     if post is None:
         return "글을 찾을 수 없습니다.", 404
+
+    # 날짜를 문자열로 변환
+    if isinstance(post['date'], datetime):
+        post['date_str'] = post['date'].strftime('%Y-%m-%d %H:%M:%S')
+    else:
+        post['date_str'] = post['date']
+
     return render_template('post_detail.html', post=post)
+
 
 @app.route('/write')
 def write():
@@ -45,6 +71,7 @@ def create_post():
     title = request.form['title']
     content = request.form['content']
     image_url = ''
+    date = datetime.now()  # 날짜를 datetime 객체로 저장
 
     if 'image' in request.files:
         image = request.files['image']
@@ -55,9 +82,10 @@ def create_post():
             image_url = f'/static/uploads/{filename}'
     
     new_id = max(post['id'] for post in posts) + 1 if posts else 1
-    posts.append({'id': new_id, 'title': title, 'content': content, 'image_url': image_url, 'date': 'MARCH 20, 2025'})
+    posts.append({'id': new_id, 'title': title, 'content': content, 'image_url': image_url, 'date': date})
     
     return redirect('/')
+
 
 
 @app.route('/delete/<int:post_id>', methods=['DELETE'])
