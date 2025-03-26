cac07c6e20b72d96e6279a35b01d0b65d905188b
Daewon-0 <sodagraph@gmail.com>
2025-03-26 13:22:55 +0900

    fix: 기본 포트를 5000에서 8000으로 변경

--- insta/insta/blog_test.py
+++ insta/insta/blog_test.py
@@ -167,5 +167,5 @@ def apply_filter():
     return jsonify({'filtered_image': data_url})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
